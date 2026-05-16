b29123e27f02c384552b98c58363e2fbd6c31469
rocketmike12 <mg.korop@gmail.com>
2026-05-16 14:32:45 +0300

    dark mode implemented; Header done

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,33 +1,55 @@
+import { useState, type ChangeEvent } from "react";
+
 import { Container } from "../Container/Container";
 
-import logo from "/src/img/logo.svg";
+import logoLight from "/src/img/logo-light.svg";
+import logoDark from "/src/img/logo-dark.svg";
 
 import styles from "./Header.module.scss";
 
 export const Header = function () {
+	const [dark, setDark] = useState(false);
+
+	const handleDarkMode = (e: ChangeEvent<HTMLInputElement>) => {
+		console.log(e.currentTarget.value);
+		if (e.currentTarget.checked) {
+			document.documentElement.classList.add("dark");
+			setDark(true);
+		} else {
+			document.documentElement.classList.remove("dark");
+			setDark(false);
+		}
+	};
+
 	return (
 		<>
-		<header className={styles["header"]}>
-			<Container>
-				<img src={logo} alt="coding magic logo" className={styles["logo"]} />
-				<nav className={styles["header-nav"]}>
-					<a className={styles["header-nav-link"]} href="">Інтерактив</a>
-					<a className={styles["header-nav-link"]} href="">Наша команда</a>
-					<a className={styles["header-nav-link"]} href="">Контакти</a>
-				</nav>
-				<ul className={styles["header-settings-list"]}>
-					<li className={styles["header-settings-list-item"]}>
-						<label className={styles["header-settings-list-switch"]}>
-							<input type="checkbox" className={styles["header-settings-list-switch-checkbox"]} />
-							<span className={styles["header-settings-list-switch-slider"]}></span>
-						</label>
-					</li>
-					<li className={styles["header-settings-list-item"]}>
-						<p className={styles["header-settings-list-text"]}>Вітаємо, User!</p>
-					</li>
-				</ul>
-			</Container>
-</header>
+			<header className={styles["header"]}>
+				<Container>
+					<img src={dark ? logoDark : logoLight} className={styles["logo"]} />
+					<nav className={styles["header-nav"]}>
+						<a className={styles["header-nav-link"]} href="">
+							Інтерактив
+						</a>
+						<a className={styles["header-nav-link"]} href="">
+							Наша команда
+						</a>
+						<a className={styles["header-nav-link"]} href="">
+							Контакти
+						</a>
+					</nav>
+					<ul className={styles["header-settings-list"]}>
+						<li className={styles["header-settings-list-item"]}>
+							<label className={styles["header-settings-list-switch"]}>
+								<input type="checkbox" className={styles["header-settings-list-switch-checkbox"]} onChange={handleDarkMode} />
+								<span className={styles["header-settings-list-switch-slider"]}></span>
+							</label>
+						</li>
+						<li className={styles["header-settings-list-item"]}>
+							<p className={styles["header-settings-list-text"]}>Вітаємо, User!</p>
+						</li>
+					</ul>
+				</Container>
+			</header>
 		</>
 	);
 };
