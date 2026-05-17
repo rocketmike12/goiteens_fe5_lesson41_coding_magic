f21f121bd317dc586c191cb83115d59e19171fb8
rocketmike12 <mg.korop@gmail.com>
2026-05-17 12:41:03 +0300

    Calculator done

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { Header } from "./components/Header/Header";
 import { Hero } from "./components/Hero/Hero";
+import { Calculator } from "./components/Calculator/Calculator";
 
 export const App = function () {
 	return (
 		<>
 			<Header />
 			<Hero />
+			<Calculator />
 		</>
 	);
 };
